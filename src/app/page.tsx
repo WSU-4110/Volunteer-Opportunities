4f72bab9f9e8aaabd9863dbcfcd23dfe7de82577
Jacob Ulbrich <hm2648@wayne.edu>
2024-09-27 11:25:28 -0400

    Added changes to personnal branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Link from "next/link"
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="relative">
+      <div className="opacity-50">
+        <img src="volunteer-image.jpg" width="2000"></img>
+      </div>
+      <div className="font-mono absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        Center Text
+      </div>
     </div>
   );
 }
