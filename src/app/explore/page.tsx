25f6d14c49030e47d153607bee5e1b3bbfa47950
Sebastian Newberry <sebasn327@gmail.com>
2024-11-19 01:32:31 -0500

    deleted a console.log statement

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,17 +1,11 @@
-import { useEffect, useState } from "react";
 import { getAllSkills, getListings, getUser } from "./actions";
 
-import Listing from "./(components)/Listing";
 import Userpage from "./(components)/Userpage";
-
-import ListingInterface from "./(components)/Userpage";
 
 export default async function Explore() {
   const [listings, listingsError] = await getListings();
   const [skills, skillsError] = await getAllSkills();
   const [userId, getUserError] = await getUser();
-
-  console.log(listings);
 
   return (
     <>
