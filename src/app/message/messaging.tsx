82a415706cb4ff4116a9e995315cc0051374b531
Sebastian Newberry <sebasn327@gmail.com>
2024-11-26 12:31:37 -0500

    added pusher connections for realtime messaging

--- src/app/message/messaging.tsx
+++ src/app/message/messaging.tsx
@@ -10,6 +10,7 @@ import MessageInput from "./messageInput";
 import BackIcon from "@/components/icons/backIcon";
 import Link from "next/link";
 import { pusherClient } from "@/lib/pusher-client";
+import { connectPusher } from "@/lib/pusher-client";
 
 const Messaging = ({
   userStatus,
@@ -218,8 +219,10 @@ const Messaging = ({
 
     setLoadingFalse();
   };
+
   useEffect(() => {
-    const subscribeToConversations = () => {
+    const subscribeToConversations = async () => {
+      await connectPusher();
       conversations.forEach((conversation: any) => {
         if (!conversationIds.includes(conversation.conversations.id)) {
           pusherClient.subscribe(conversation.conversations.id);
