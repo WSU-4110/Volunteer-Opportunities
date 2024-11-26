2203d00187cf8e72caf46fdf2a9ece02b85b4f12
Sebastian Newberry <sebasn327@gmail.com>
2024-11-26 13:14:25 -0500

    attempt to fix pusher not working on production on our website

--- src/lib/pusher-client.ts
+++ src/lib/pusher-client.ts
@@ -16,8 +16,9 @@ export const pusherClient = new PusherClient(
   }
 );
 
-export const connectPusher = () =>
-  new Promise((resolve, reject) => {
+export const connectPusher = async () => {
+  console.log("this ran");
+  await new Promise((resolve, reject) => {
     pusherClient.connection.bind("connected", () => {
       console.log("Pusher connected successfully");
       resolve(true);
@@ -28,3 +29,4 @@ export const connectPusher = () =>
       reject(err);
     });
   });
+};
